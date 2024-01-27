f56e709fb25e3f44ebf298b18d7cadddf5d6af50
Timofey Koolin <rekby@yandex-team.com>
2024-01-27 13:56:41 +0300

    replace work

--- main.ts
+++ main.ts
@@ -1,81 +1,58 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting, TextAreaComponent} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PasteTransformSettings {
+	patterns: string[],
+	replacers: string[],
+	settingsVersion: number,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PasteTransformSettings = {
+	patterns: [],
+	replacers: [],
+	settingsVersion: 1,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ReplaceRule {
+	pattern: RegExp;
+	replacer: string;
+
+	constructor(pattern: string, replacer: string) {
+		this.pattern = new RegExp(pattern, 'g');
+		this.replacer = replacer;
+	}
+}
+
+export default class PasteTransform extends Plugin {
+	settings: PasteTransformSettings;
+	rules: ReplaceRule[];
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerEvent(this.app.workspace.on("editor-paste", event => this.onPaste(event)));
+	}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	onPaste(event: ClipboardEvent){
+		let types = event.clipboardData?.types;
+		console.log("transform plugin, clipboard content types:", types);
+		if (types === undefined || types.length != 1 || types[0] != "text/plain"){
+			return;
+		}
+		let plainText = event.clipboardData?.getData("text/plain");
+		if (plainText === undefined || plainText == ""){
+			return;
+		}
+
+		let result = this.applyRules(plainText);
+		console.log(`Replaced '${plainText}' -> '${result}'`);
+
+		this.app.workspace.activeEditor?.editor?.replaceSelection(result);
+		event.preventDefault()
 	}
 
 	onunload() {
@@ -84,33 +61,48 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.compileRules();
+	}
+
+	compileRules()  {
+		this.rules = [];
+		let minIndex = this.settings.patterns.length;
+		if (this.settings.replacers.length < minIndex){
+			minIndex = this.settings.replacers.length;
+		}
+		for (let i = 0; i < minIndex; i++){
+			this.rules.push(
+				new ReplaceRule(this.settings.patterns[i], this.settings.replacers[i])
+			)
+		}
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	public applyRules(source: string | null | undefined) : string {
+		if (source === undefined || source === null){
+			return ""
+		}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		let result = source;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		for (let rule of this.rules){
+			if (source.search(rule.pattern) != -1) {
+				result = source.replace(rule.pattern, rule.replacer);
+				break
+			}
+		}
+
+		return result;
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: PasteTransform;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PasteTransform) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +112,94 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		let patternsTa: TextAreaComponent | null = null;
+		let replacersTa: TextAreaComponent | null = null;
+		let trySource: TextAreaComponent | null = null;
+		let tryDest: TextAreaComponent | null = null;
+
+		let plugin = this.plugin;
+		let handleChanges = function (){
+			try {
+				tryDest?.setValue(plugin.applyRules(trySource?.getValue()))
+			} catch (e) {
+				tryDest?.setValue("ERROR:\n" + e)
+			}
+		}
+
+		let handleTextChange = async function (value: string, setAttr: (values: string[]) => any ){
+			let values = value.split("\n");
+			if (values.length > 0 && values.last() == "") {
+				values.pop()
+			}
+
+			setAttr(values)
+
+			try {
+				plugin.compileRules();
+				handleChanges()
+				await plugin.saveSettings();
+			} catch (e){
+				tryDest?.setValue("ERROR:\n" + e)
+			}
+		}
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Transform rules")
+			.setDesc("Type regexp patterns in left box and replace rules in right box." +
+				"Every pattern and rule on a line." +
+				"Pattern and rules use matched by line numbers." +
+				"Regexp and replace rules use typescript rules."
+			)
+			.addTextArea(ta =>
+				{
+					patternsTa = ta;
+					patternsTa.setPlaceholder("patter 1\npattern 2\n")
+
+					let patternsString = "";
+					for (let val of this.plugin.settings.patterns){
+						patternsString += val + "\n"
+					}
+					patternsTa.setValue(patternsString)
+					patternsTa.onChange(async value=> {
+						await handleTextChange(value, values => {
+							plugin.settings.patterns = values;
+						});
+					})
+				}
+			)
+			.addTextArea(ta=>{
+				replacersTa = ta;
+				replacersTa.setPlaceholder("replacer 1\nreplacer 2\n")
+				let replacersString = "";
+				for (let val of this.plugin.settings.replacers){
+					replacersString += val + "\n"
+				}
+				replacersTa.setValue(replacersString)
+				replacersTa.onChange(async value=> {
+					await handleTextChange(value, values => {
+						plugin.settings.replacers = values;
+					});
+				})
+			})
+		;
+
+		new Setting(containerEl)
+			.setName("Try source")
+			.setDesc("Write here example of pasted text")
+			.addTextArea(ta=> {
+				trySource = ta;
+				ta.setPlaceholder("Sample text")
+				ta.onChange(_=> {
+					handleChanges()
+				})
+			});
+		new Setting(containerEl)
+			.setName("Try destination")
+			.setDesc("Here is result of apply rules to Try source text")
+			.addTextArea(ta => {
+				tryDest = ta;
+				ta.setPlaceholder("Transform result")
+				ta.setDisabled(true);
+			})
 	}
 }
